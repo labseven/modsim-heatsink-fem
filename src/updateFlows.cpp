8d5a640bde87297ad837f7c6befe63c3b3667253
Adam Selker <aselker@openmailbox.org>
2016-10-13 15:59:42 -0400

    Added updateFlows source file

---
+++ src/updateFlows.cpp
@@ -0,0 +1,15 @@
+/*
+ * updateFlows.cpp
+ *
+ *  Created on: Oct 13, 2016
+ *      Author: neophile
+ */
+
+#define NUM int32_t
+
+//Calculates flow between each pair of cells, given cell temps and materials
+bool updateFlows (NUM cellCount, *NUM temps, *NUM flows, *NUM materials, *NUM matRef );
+
+
+
+
