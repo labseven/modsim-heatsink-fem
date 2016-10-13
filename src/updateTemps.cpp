244d4c381f561d37aaa3deea64b23a93192ffaeb
Adam Yesvotny <adam.novotny55@gmail.com>
2016-10-13 17:22:00 -0400

    added temprature update code

---
+++ src/updateTemps.cpp
@@ -0,0 +1,24 @@
+/* updateTemps
+ *
+ * input: (cellCount, currentTemps[], newTemps[], flows[], materials[], matRef[])
+ * output: bool exit code
+ *
+ * */
+
+
+#include "heatsink.h"
+
+
+bool updateTemps(NUM *cellCount, NUM *currentTemps[], NUM *newTemps[], NUM *flows[], NuM materials[], matRef[])
+{
+	for(int i = 0; i < cellCount; i++)
+	{
+
+		// newTemp = currentT
+		int newEnergy = currentTemps[i]
+		newTemps[i] = currentTemps[i];
+	}
+
+
+	return true;
+}
