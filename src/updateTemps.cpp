a3f8387b3bf31eff84d81b7b12cadc9b1ca331a2
Adam Yesvotny <adam.novotny55@gmail.com>
2016-10-17 16:16:52 -0400

    completed newTemp

--- src/updateTemps.cpp
+++ src/updateTemps.cpp
@@ -8,25 +8,34 @@
 #include "updateTemps.h"
 #include "energyTempConversion.h"
 
-bool updateTemps(NUM cellCount, NUM dt, NUM currentTemps[], NUM newTemps[], NUM flows[], int materials[], material matRef[])
+NUM newTemp(NUM currentTemp, NUM deltaTime, int inMaterial, material matRef[], NUM flow)
+{
+	NUM newEnergy, newTemperature, currentEnergy;
+	material thisMaterial;
+
+	thisMaterial = &matRef[inMaterial];
+	currentEnergy = temp2energy(currentTemp, thisMaterial->heatCapacity);
+
+
+	newEnergy = energy + (flow * deltaTime);
+
+	newTemperature = energy2temp(newEnergy, thisMaterial->heatCapacity);
+
+
+	if (newTemprature < 0){
+		std::cout << "Warning: new temp is <0. (" << newTemprature << ")\n";
+	}
+	return newTemperature;
+}
+
+bool updateTemps(NUM cellCount, NUM deltaTime, NUM currentTemps[], NUM newTemps[], NUM flows[], int materials[], material matRef[])
 {
 	NUM energy, flow;
-	material *material;
+	material *thisMaterial;
 
 	for(int i = 0; i < cellCount-1; i++) // Loop through all cells
 	{
-		material = &matRef[materials[i]];
-		energy = temp2energy(currentTemps[i], material->heatCapacity);
-
-
-
-
-
-		// newTemp = currentT
-		//int newEnergy = currentTemps[i]
-		//newTemps[i] = c2energy(currentTemps[i], ;
-
-
+		newTemps[i] = newTemp(currentTemps[i], deltaTime, materials[i], matRef, flows[i]);
 	}
 
 
