e66413dbb4452ece2048c3b0dda92ba293d2e7ed
Adam Yesvotny <adam.novotny55@gmail.com>
2016-10-17 08:37:25 -0400

    comments

--- src/energyTempConversion.cpp
+++ src/energyTempConversion.cpp
@@ -7,24 +7,24 @@
 
 #include "energyTempConversion.h"
 
-NUM energy2temp (NUM energy, NUM heatCapacity)
+NUM energy2temp (NUM energy, NUM heatCapacity) /* energy J to Temperature Units */
 {
 	return energy*1.0 / heatCapacity;
 }
 
-NUM temp2energy (NUM temp, NUM heatCapacity)
+NUM temp2energy (NUM temp, NUM heatCapacity) /* Temperature Units */
 {
 	return temp * heatCapacity;
 
 }
 
 
-NUM units2c(NUM units)
+NUM units2c(NUM units) /* units to Celcius */
 {
 	return units / UNITSPERC;
 }
 
-NUM c2units(NUM c)
+NUM c2units(NUM c) /* Celcius to units */
 {
 	return c * UNITSPERC;
 }
