6de654e86eae862350c6d7c1ceb13f1a92118391
Adam Yesvotny <adam.novotny55@gmail.com>
2016-10-17 15:42:46 -0400

    changed fixedpoint to NUM

--- src/updateTemps.cpp
+++ src/updateTemps.cpp
@@ -6,15 +6,25 @@
  * */
 
 #include "updateTemps.h"
+#include "energyTempConversion.h"
 
 bool updateTemps(NUM cellCount, NUM currentTemps[], NUM newTemps[], NUM flows[], NUM materials[], material matRef[])
 {
-	for(int i = 0; i < cellCount; i++)
+	fixpoint energy, flow;
+	material *material;
+
+	for(int i = 0; i < cellCount-1; i++) // Loop through all cells
 	{
+		material = &matRef[materials[i]];
+		energy = temp2energy(currentTemps[i], material->heatCapacity);
+
+
 
 		// newTemp = currentT
 		//int newEnergy = currentTemps[i]
-		//newTemps[i] = currentTemps[i];
+		//newTemps[i] = c2energy(currentTemps[i], ;
+
+
 	}
 
 
