4cfbf997bd6c400b080dd5fa9057ad9036958e5c
Adam Yesvotny <adam.novotny55@gmail.com>
2016-10-24 18:21:08 -0400

    3D updateTemps framework

--- src/updateTemps.cpp
+++ src/updateTemps.cpp
@@ -34,7 +34,7 @@ NUM newTemp(NUM currentTemp, NUM deltaTime, int inMaterial, material matRef[], N
 	return newTemperature;
 }
 
-NUM newTemp2D(NUM currentTemp, NUM deltaTime, int inMaterial, material matRef[], NUM flowX1, NUM flowX2,NUM flowY1, NUM flowY2)
+NUM newTemp2D(NUM currentTemp, NUM deltaTime, int inMaterial, material matRef[], NUM flowX1, NUM flowX2, NUM flowY1, NUM flowY2)
 {
 
 	NUM newEnergy, newTemperature, currentEnergy;
@@ -91,4 +91,51 @@ bool updateTemps2D (NUM deltaTime, NUM currentTemps[MAP_Y][MAP_X], NUM newTemps[
 
 	return true;
 
+}
+
+NUM newTemp3D(NUM currentTemp, NUM deltaTime, int inMaterial, material matRef[], NUM flowX1, NUM flowX2, NUM flowY1, NUM flowY2, NUM flowZ1, NUM flowZ2)
+{
+	NUM newEnergy, newTemperature, currentEnergy;
+	material *thisMaterial;
+
+	thisMaterial = &matRef[inMaterial];
+	if(thisMaterial->constantTemp){
+		if(DEBUG) std::cout << "updateTemp2D: constantTemp, will not change\n";
+		return (thisMaterial->tempSetPoint);
+	}
+
+	currentEnergy = temp2energy(currentTemp, thisMaterial->heatCapacity);
+
+	newEnergy = currentEnergy + (((flowX1-flowX2) + (flowY1-flowY2) + (flowZ1-flowZ2)) * deltaTime); // Flows move left to right, up to down; add left/up, subtract right/down
+
+
+	newTemperature = energy2temp(newEnergy, thisMaterial->heatCapacity);
+
+	if(DEBUG) std::cout << "updateTemp2D: currE: " << currentEnergy << "J flowX1: " << flowX1 << "W flowX2: " << flowX2 << "W flowY1: " << flowY1 << "W flowY2: " << flowY2 << "W flowZ1: " << flowZ1 << "W flowZ2: " << flowZ2 << "W  newE: " << newEnergy << "J (dt: " << deltaTime << ") ";
+	if(DEBUG) std::cout << "new temp: " << newTemperature << "C\n";
+
+	if (newTemperature < 0){
+		if(DEBUG) std::cout << "Warning: new temp is < 0. (" << newTemperature << ")\n";
+	}
+	return newTemperature;
+
+}
+
+bool updateTemps3D (NUM deltaTime, NUM currentTemps[MAP_Z][MAP_Y][MAP_X], NUM newTemps[MAP_Z][MAP_Y][MAP_X], NUM flowsX[MAP_Z][MAP_Y][MAP_X-1], NUM flowsY[MAP_X][MAP_Z][MAP_Y-1], NUM flowsZ[MAP_Y][MAP_X][MAP_Z-1], int materials[MAP_Z][MAP_Y][MAP_X], material matRef[])
+{
+	if(DEBUG) std::cout << "\nupdateTemps3D: \n";
+
+	for(int z = 0; z < MAP_Z; z++) // Loop through all cells
+	{
+		for(int x = 0; x < MAP_X; x++) // Loop through all cells
+		{
+			for(int y = 0; y < MAP_Y; y++)
+			{
+				newTemps[z][y][x] = newTemp3D(currentTemps[z][y][x], deltaTime, materials[z][y][x], matRef, flowsX[z][y][x-1], flowsX[z][y][x], flowsY[z][x][y-1], flowsY[z][x][y], flowsZ[z][x][y], flowsZ[z][x][y]); // Errors will occur here from picking wrong flowsX, flowsY, and flowsZ values
+			}
+		}
+	}
+
+
+	return true;
 }
