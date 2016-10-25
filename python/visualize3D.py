ad1fffe1156c6afac5b986f5db5d74979cd721a1
Adam Yesvotny <adam.novotny55@gmail.com>
2016-10-25 16:30:42 -0400

    python imports data successfully

--- python/visualize3D.py
+++ python/visualize3D.py
@@ -1,13 +1,40 @@
 # Imports the output into python and then visualize it
 
+
+# tempraturesTimeArray holds [iteration#][0: time, 1+:z level][y level]
+
 with open("outputPython1.csv","r") as fileIn:
 	currLine = fileIn.readline();
 
 	xyzSize = [int(n) for n in currLine.rstrip('\n').split(",")];
 
 	print (xyzSize);
+	#print(xyzSize[1]);
+	tempraturesArray = [[[] for i in range(xyzSize[1])] for i in range(xyzSize[2])];
+	tempraturesTimeArray = [];
+	# print (tempraturesArray);
 
-	for z in range(xyzSize[2]):
-		for y in range(xyzSize[1]):
-			for x in range(xyzSize[0]):
-				print("Line",x,":", fileIn.readline());
+	while(1):
+		currLine = fileIn.readline();
+		if(not currLine): break;
+		time = [int(currLine.rstrip('\n'))];
+		print("Time: ", time);
+
+		for z in range(xyzSize[2]):
+			for y in range(xyzSize[1]):
+				currLine = fileIn.readline();
+				bigAssList = currLine.split(",");
+				bigAssList.pop(); # removes the last element, which is \n or empty
+				bigAssList = [float(n) for n in bigAssList];
+				#print("Line",z, y,":", bigAssList);
+				tempraturesArray[z][y] = bigAssList;
+		print("Tempratures: ",tempraturesArray);
+		tempraturesTimeArray.append(time + tempraturesArray);
+
+print;
+print("Time and temp: ", tempraturesTimeArray);
+
+print("Time of first thing: ", tempraturesTimeArray[0][0]);
+print("Tempratures of 2nd z level, 2nd row of first thing: ", tempraturesTimeArray[0][2][1]);
+
+print("Done!");
